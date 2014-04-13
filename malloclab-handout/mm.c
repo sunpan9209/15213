eccae778ce9f94ba7bbb7391614223fedffb3a3f
pan sun <you@example.com>
2014-04-13 06:57:38 -0400

    update

--- malloclab-handout/mm.c
+++ malloclab-handout/mm.c
@@ -11,8 +11,10 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <string.h>
 #include <unistd.h>
+#include "contracts.h"
 
 #include "mm.h"
 #include "memlib.h"
